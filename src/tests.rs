fb78e0f33d499f67d18f3c40bcdd0b1d045b1157
Without Boats <woboats@gmail.com>
2020-05-25 13:15:43 +0200

    More tests

--- src/tests.rs
+++ src/tests.rs
@@ -16,3 +16,48 @@ fn test_release() {
     queue.release(1);
     assert_eq!(queue.count.load(SeqCst), 1);
 }
+
+#[test]
+fn holds_accesses() {
+    let mut ctx = Context::from_waker(futures_task::noop_waker_ref());
+    let queue = AccessQueue::new((), 1);
+
+    let mut a1_f = queue.access();
+    let mut a2_f = queue.access();
+
+    let a1 = Pin::new(&mut a1_f).poll(&mut ctx);
+    assert!(matches!(&a1, &Poll::Ready(_)));
+
+    let a2 = Pin::new(&mut a2_f).poll(&mut ctx);
+    assert!(matches!(&a2, &Poll::Pending));
+
+    drop(a1);
+
+    let a2 = Pin::new(&mut a2_f).poll(&mut ctx);
+    assert!(matches!(&a2, &Poll::Ready(_)));
+}
+
+#[test]
+fn releases_accesses_fifo() {
+    let mut ctx = Context::from_waker(futures_task::noop_waker_ref());
+    let queue = AccessQueue::new((), 0);
+
+    let mut a1_f = queue.access();
+    let mut a2_f = queue.access();
+
+    let a1 = Pin::new(&mut a1_f).poll(&mut ctx);
+    assert!(matches!(&a1, &Poll::Pending));
+
+    let a2 = Pin::new(&mut a2_f).poll(&mut ctx);
+    assert!(matches!(&a2, &Poll::Pending));
+
+    queue.release(1);
+
+    let a1 = Pin::new(&mut a1_f).poll(&mut ctx);
+    assert!(matches!(&a1, &Poll::Ready(_)));
+
+    queue.release(1);
+
+    let a2 = Pin::new(&mut a2_f).poll(&mut ctx);
+    assert!(matches!(&a2, &Poll::Ready(_)));
+}
